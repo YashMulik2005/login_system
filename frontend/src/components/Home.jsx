3e8f7b9db720f9b7005ac2358451749458764874
Your Name <you@example.com>
2023-06-11 19:01:07 +0530

    dark mode done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 function Home() {
     const [loginStatus, setloginStatus] = useState(false)
     const [logedinName, setlogedinName] = useState("")
+    const [theme, settheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light")
     axios.defaults.withCredentials = true;
     useEffect(() => {
         const getusername = async () => {
@@ -17,7 +18,10 @@ function Home() {
             }
         }
         getusername()
-    }, [])
+        localStorage.setItem("theme", theme)
+        const localtheme = localStorage.getItem("theme")
+        document.querySelector('html').setAttribute("data-theme", localtheme)
+    }, [theme])
 
     const data = {
         "username": logedinName
@@ -28,10 +32,24 @@ function Home() {
         setlogedinName("");
     }
 
+    const changetheme = () => {
+        if (theme == "light") {
+            settheme("dark");
+        }
+        else {
+            settheme("light");
+        }
+    }
+
     return (
         <div>
             <h1 className=''>welcome {logedinName}</h1>
-            <button className=' bg-blue-600 py-1 px-2 text-white rounded' onClick={handlelogout}>LOGOUT</button>
+            <button className=' bg-blue-600 py-1 px-2 text-white rounded m-1' onClick={handlelogout}>LOGOUT</button>
+            <button className=' bg-blue-600 py-1 px-2 text-white rounded m-1' onClick={changetheme}>theme change</button>
+            <div className={`w-1/2 border-2 m-4 p-4 ${theme == "dark" ? "bg-black" : "bg-white"} `}>
+                <h1>Hii,I am Yash Mulik</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad eos vero asperiores accusamus quod velit eaque voluptates maiores molestias voluptatibus, ullam modi, atque laudantium deserunt dicta! Mollitia omnis exercitationem officia.</p>
+            </div>
         </div>
     )
 }
