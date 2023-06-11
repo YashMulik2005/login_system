73a3aaf78ffd5fdc133b4a18717c2ae713bb218d
Your Name <you@example.com>
2023-06-11 11:50:27 +0530

    logout done

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,30 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const bodyPerser = require('body-parser')
-
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bodyPerser = require("body-parser");
 
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials:true
+    credentials: true,
   })
 );
-app.use(bodyPerser.json())
-app.use(cookieParser())
+app.use(bodyPerser.json());
+app.use(cookieParser());
 app.use(express.json());
-app.use(session({
-  secret:'secret',
-  resave:false,
-  saveUninitialized: false,
-  cookie:{
-    secure:false,
-    maxAge: 1000 * 60 * 60 * 24
-  }
-}))
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 const con = mysql.createConnection({
   host: "localhost",
@@ -45,21 +46,32 @@ app.listen(3000, () => {
   console.log("server is running.");
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   console.log(req.session.username);
-  if(req.session.username){
+  if (req.session.username) {
     return res.status(200).json({
-      data:{  sucess: true,
-              username: req.session.username
-      }
-    })
-  }
-  else{
+      data: { sucess: true, username: req.session.username },
+    });
+  } else {
     return res.status(200).json({
-      data:{ sucess:false }
-    })
+      data: { sucess: false },
+    });
   }
-})
+});
+
+app.post("/", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(200).json({
+        data: { sucess: ture },
+      });
+    } else {
+      return res.status(200).json({
+        data: { sucess: false },
+      });
+    }
+  });
+});
 
 app.post("/login", (req, res) => {
   const { data } = req.body;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Home() {
@@ -6,22 +6,32 @@ function Home() {
     const [logedinName, setlogedinName] = useState("")
     axios.defaults.withCredentials = true;
     useEffect(() => {
-        const getusername = async()=>{
+        const getusername = async () => {
             // const result = await fetch('http://localhost:3000/')
             // const res = await result.json()
             const result = await axios.get('http://localhost:3000/')
             console.log(result.data)
-            if(result.data.data.sucess){
+            if (result.data.data.sucess) {
                 setloginStatus(true)
                 setlogedinName(result.data.data.username)
             }
         }
         getusername()
     }, [])
-    
+
+    const data = {
+        "username": logedinName
+    }
+    const handlelogout = async () => {
+        const result = await axios.post('http://localhost:3000/', { "data": data })
+        console.log(result);
+        setlogedinName("");
+    }
+
     return (
         <div>
             <h1 className=''>welcome {logedinName}</h1>
+            <button className=' bg-blue-600 py-1 px-2 text-white rounded' onClick={handlelogout}>LOGOUT</button>
         </div>
     )
 }
